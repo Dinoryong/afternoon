0f827ef5ccebb2f41eefdc756766b35eef3aa95e
최재웅 <jason967@naver.com>
2021-01-18 22:52:45 +0900

    [S04P12A302-50] setup : set base architecture and test

---
+++ backend/src/test/java/com/a302/webcuration/controller/SampleControllerTest.java
@@ -0,0 +1,30 @@
+package com.a302.webcuration.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    private MockMvc mock;
+
+    @Test
+    public void Sample_리턴() throws Exception {
+
+        String sample = "sample";
+
+        mock.perform(get("/sample"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(sample));
+    }
+}
