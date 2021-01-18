c5e8d5328eaa4847299488e1673fa530bfddce39
Nanjae <dngngn3045@gmail.com>
2021-01-18 20:35:58 +0900

    [S04P12A302-3] init : upload base project

---
+++ backend/src/test/java/com/a302/webcuration/WebcurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a302.webcuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
