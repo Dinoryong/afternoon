638e504415c329f1eee7408445785d4154195feb
Nanjae <dngngn3045@gmail.com>
2021-01-18 23:46:59 +0900

    [S04P12A302-19] setup : set project folder structure

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.scss";
+import Header from "../components/Header";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
