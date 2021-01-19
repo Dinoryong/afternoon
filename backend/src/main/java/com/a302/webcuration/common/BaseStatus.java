985d229ceadfd360eb043b07d12a2c160e2c3f58
최재웅 <jason967@naver.com>
2021-01-19 09:06:42 +0900

    [S04P12A302-51] setup : set directory and base

--- backend/src/main/java/com/a302/webcuration/common/BaseStatus.java
+++ backend/src/main/java/com/a302/webcuration/common/BaseStatus.java
@@ -10,6 +10,6 @@ public enum BaseStatus {
     NOT_FOUND(404, "NOT_FOUND"),
     INTERNAL_SERER_ERROR(500, "INTERNAL_SERVER_ERROR");
 
-    int statusCode;
-    String code;
+    int httpStatusCode;
+    String httpStatus;
 }
