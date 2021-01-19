28f5c62e5768b45f6dbcbb0d3ed91bcaed47b4cf
dntjr4772 <dntjr4772@naver.com>
2021-01-19 11:08:49 +0900

    [S04P12A302-123] feat : add domain

--- backend/src/main/java/com/a302/webcuration/domain/Tag/Tag.java
+++ backend/src/main/java/com/a302/webcuration/domain/Tag/Tag.java
@@ -1,16 +1,27 @@
 package com.a302.webcuration.domain.Tag;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Getter @NoArgsConstructor @AllArgsConstructor
+@Getter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Tag {
-    @Column(name = "tag_id")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long tagId;
+    private String tagTitle;
+    private String tagDesc;
+    private int tagUserCount;
+    @ManyToOne
+    private Tag parent;
+    @OneToMany(mappedBy = "parent")
+    @Builder.Default
+    private List<Tag> child=new ArrayList<>();
+
 }
