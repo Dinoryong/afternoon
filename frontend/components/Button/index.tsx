93cdcb0067a7afd0f4ee76ee8a8fbd4bfb7724a3
Nanjae <dngngn3045@gmail.com>
2021-01-19 15:35:55 +0900

    [S04P12A302-44] feat : create button component and theme.js

--- frontend/components/Button/index.tsx
+++ frontend/components/Button/index.tsx
@@ -1,31 +1,93 @@
 import React from "react";
 import styled from "@emotion/styled";
+import color from "../../styles/theme";
 
-const Container = styled.div`
+type HoverProps = {
+  btnHoverBorderColor?: string;
+  btnHoverTextColor?: string;
+  btnHoverBgColor?: string;
+};
+
+const Container = styled.div<HoverProps>`
   display: flex;
+  justify-content: center;
   align-items: center;
-  border-radius: 4px;
-  border: 1px solid #d1d1d1;
-  margin-right: 18px;
-  height: 32px;
+  border: 1px solid;
   min-width: 65px;
-  padding: 0px 10px;
   cursor: pointer;
-  :hover {
-    border-color: #111111;
-    color: #111111;
-  }
   transition: all 0.2s;
+
+  width: 65px;
+  height: 32px;
+  color: ${color.gray.dark};
+  background-color: ${color.white.default};
+  border-color: ${color.gray.default};
+  border-radius: 4px;
+  margin-left: 8px;
+  margin-right: 8px;
+  font-weight: 300;
+
+  :hover {
+    border-color: ${(props: HoverProps) => props.btnHoverBorderColor};
+    background-color: ${(props: HoverProps) => props.btnHoverBgColor};
+    color: ${(props: HoverProps) => props.btnHoverTextColor};
+  }
 `;
 
 type ButtonProps = {
   btnText?: string;
   btnBgColor?: string;
+  btnBorderColor?: string;
+  btnTextColor?: string;
+  btnWidth?: string;
+  btnHeight?: string;
+  btnMarginLeft?: string;
+  btnMarginRight?: string;
+  btnBorderRadius?: string;
+  btnFontWeight?: number;
+  btnHoverTextColor?: string;
+  btnHoverBorderColor?: string;
+  btnHoverBgColor?: string;
 };
 
-const index = ({ btnText, btnBgColor }: ButtonProps) => {
+const index = ({
+  btnText = "버튼",
+  btnBgColor,
+  btnTextColor,
+  btnBorderColor,
+  btnWidth,
+  btnHeight,
+  btnBorderRadius,
+  btnMarginLeft,
+  btnMarginRight,
+  btnFontWeight,
+  btnHoverTextColor = color.black.default,
+  btnHoverBorderColor = color.black.default,
+  btnHoverBgColor,
+}: ButtonProps) => {
+  const props = {
+    btnHoverTextColor,
+    btnHoverBorderColor,
+    btnHoverBgColor,
+  };
+
   return (
-    <Container style={{ backgroundColor: btnBgColor }}>{btnText}</Container>
+    <Container
+      style={{
+        backgroundColor: btnBgColor,
+        color: btnTextColor,
+        borderColor: btnBorderColor,
+        width: btnWidth,
+        height: btnHeight,
+        borderRadius: btnBorderRadius,
+        marginLeft: btnMarginLeft,
+        marginRight: btnMarginRight,
+        fontWeight: btnFontWeight,
+      }}
+      {...props}
+    >
+      {btnText}
+    </Container>
   );
 };
 
