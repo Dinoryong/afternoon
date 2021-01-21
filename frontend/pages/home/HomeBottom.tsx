b2c7b6d8c092d6ca3b777b876a4fc5621e674835
Nanjae <dngngn3045@gmail.com>
2021-01-21 19:55:33 +0900

    [S04P12A302-128] feat : create home page and modify header min-width

---
+++ frontend/pages/home/HomeBottom.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 1200px;
+`;
+
+const HomeBottom = () => {
+  return <Container></Container>;
+};
+
+export default HomeBottom;
