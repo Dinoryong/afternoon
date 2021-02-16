be5872fb1dee11101f3b7c62872c7e9d09e7f228
Nanjae <dngngn3045@gmail.com>
2021-02-16 16:54:19 +0900

    deploy : add cors origin

--- backend/src/main/java/com/a302/webcuration/configs/WebMvcConfig.java
+++ backend/src/main/java/com/a302/webcuration/configs/WebMvcConfig.java
@@ -12,8 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
-                .allowedOrigins("http://localhost:3000", "http://118.34.136.73:8080")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://118.34.136.73:8080", "http://i4a302.p.ssafy.io:3000","http://i4a302.p.ssafy.io:8080")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
